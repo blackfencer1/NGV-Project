9d3ca10a3e4cbdb024b867525053b24fb52aa171
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-02-08 19:43:06 +0900

    .

--- project_version2/main_hj.py
+++ project_version2/main_hj.py
@@ -8,7 +8,8 @@ import numpy as np
 import threading
 from threading import Thread
 import time
-import imagepreprocess as ipp
+import ImagePreProcess as ipp
+import socket
 
 # 전역변수
 frame = np.zeros(shape=(480, 640, 3), dtype="uint8")
@@ -34,7 +35,7 @@ def main():
 
     while True:
         ret, frame = cam.read()
-        sframe.copyTo(frame)
+        frame_s.copyTo(frame)
 
         #frame_resize = cv2.resize(_frame, (800, 480), interpolation=cv2.INTER_CUBIC)
         cv2.imshow("frame", frame)
@@ -44,27 +45,27 @@ def main():
         if cv2.waitKey(1) > 0:
             break
 
-    Cam.release()
+    cam.release()
     cv2.destroyAllWindows()
 
 
-# 라인검출 스레드
+# Lane Detecting Thread
 class DetectLane(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.frame = frame
-        self.frame_edge = frame_edge
+        self.frame = np.zeros(shape=(480, 640, 3), dtype="uint8")
+        print("[Thread] Generate Image(DetectLane)")
 
     def run(self):
-        global frame_edge
         while True:
+            global frame_edge
+            global frame
             self.frame = frame
             self.frame = ipp.filter_edge(self.frame)
             img_hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(img_hsv, np.array([0, 0, 100]), np.array([255, 255, 255]))
-            self.frame_edge = cv2.bitwise_and(self.frame, self.frame, mask=mask)
-            frame_edge = self.frame_edge
-            time.sleep(0.001)
+            frame_edge = cv2.bitwise_and(self.frame, self.frame, mask=mask)
+            time.sleep(0.01)
 
     def shutdown(self):
         pass
