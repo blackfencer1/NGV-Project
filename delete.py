240ece5ab12169153b44880fcc800e274e42d9b6
ImyourGenie <leyujin1007@naver.com>
2020-02-13 04:53:34 +0900

    Update delete.py
    Firebase에서 true,false받아서 음성파일 실행하는 코드

--- delete.py
+++ delete.py
@@ -2,12 +2,11 @@ import os, time
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication("https://blackice-proj.firebaseio.com/", None)
-result = firebase.get('/Detection',None)
 
 while True:
+    result = firebase.get('/Detection', isDetect)
     if result==True:
         print("detect O")
         os.system("mpg321 text.mp3")
-        time.sleep(4)
     else:
         print("detect X")
